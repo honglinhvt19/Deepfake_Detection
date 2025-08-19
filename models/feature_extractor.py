64efd9c9af60b06d7a2d5ac021b87f58f0a3927f
honglinhdayne <honglinhvt19@gmail.com>
2025-08-19 23:04:11 +0700

    Fix

--- models/feature_extractor.py
+++ models/feature_extractor.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow import keras
+import keras
 from keras.layers import Layer, GlobalAveragePooling2D
 from keras.applications import EfficientNetB0, Xception
 
