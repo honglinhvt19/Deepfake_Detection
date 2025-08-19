03d4e14c1883b99db8758467bff23ee048e68cf1
honglinhdayne <honglinhvt19@gmail.com>
2025-08-19 23:42:19 +0700

    fix

--- models/feature_extractor.py
+++ models/feature_extractor.py
@@ -1,42 +1,67 @@
 import tensorflow as tf
-import keras
-from keras.layers import Layer, GlobalAveragePooling2D
-from keras.applications import EfficientNetB0, Xception
+from tensorflow import keras
+from keras.layers import TimeDistributed, GlobalAveragePooling2D
 
-@keras.saving.register_keras_serializable(package="Custom")
-class FeatureExtractor(Layer):
+class FeatureExtractor(keras.layers.Layer):
     def __init__(self, freeze_ratio=1.0, **kwargs):
         super().__init__(**kwargs)
-        self.freeze_ratio = freeze_ratio
 
-        self.eff = EfficientNetB0(include_top=False, weights="imagenet", pooling="avg")
-        self.xcep = Xception(include_top=False, weights="imagenet", pooling="avg")
+        # Backbone 1: Xception
+        base_xcep = keras.applications.Xception(
+            include_top=False,
+            weights="imagenet",
+            pooling=None,
+            input_shape=(224, 224, 3)
+        )
+        self.xception = keras.Sequential([
+            base_xcep,
+            GlobalAveragePooling2D()
+        ])
 
-        for model in [self.eff, self.xcep]:
-            num_layers = len(model.layers)
-            freeze_until = int(num_layers * self.freeze_ratio)
-            for layer in model.layers[:freeze_until]:
-                layer.trainable = False
-            for layer in model.layers[freeze_until:]:
-                layer.trainable = True
+        # Backbone 2: EfficientNetB0
+        base_eff = keras.applications.EfficientNetB0(
+            include_top=False,
+            weights="imagenet",
+            pooling=None,
+            input_shape=(224, 224, 3)
+        )
+        self.efficientnet = keras.Sequential([
+            base_eff,
+            GlobalAveragePooling2D()
+        ])
+
+        # Wrap bằng TimeDistributed để áp dụng cho từng frame
+        self.xception_td = TimeDistributed(self.xception)
+        self.efficientnet_td = TimeDistributed(self.efficientnet)
+
+        # Freeze tỷ lệ backbone (theo freeze_ratio)
+        self._freeze_backbones(freeze_ratio)
+
+    def _freeze_backbones(self, freeze_ratio):
+        def freeze_layers(model, ratio):
+            n_layers = len(model.layers[0].layers)  # lấy backbone gốc
+            n_freeze = int(n_layers * ratio)
+            for i, layer in enumerate(model.layers[0].layers):
+                layer.trainable = False if i < n_freeze else True
+
+        freeze_layers(self.xception_td, freeze_ratio)
+        freeze_layers(self.efficientnet_td, freeze_ratio)
 
     def call(self, inputs, training=False):
-        batch_size = tf.shape(inputs)[0]
-        num_frames = tf.shape(inputs)[1]
-        inp_flat = tf.reshape(inputs, [-1, 224, 224, 3])  # [B*T, H, W, C]
-
-        eff_feat = self.eff(inp_flat, training=training)   # (B*T, 1280)
-        xcep_feat = self.xcep(inp_flat, training=training) # (B*T, 2048)
-
-        eff_feat = tf.reshape(eff_feat, [batch_size, num_frames, -1])
-        xcep_feat = tf.reshape(xcep_feat, [batch_size, num_frames, -1])
+        # inputs: [B, T, H, W, C]
+        xcep_feat = self.xception_td(inputs, training=training)       # [B, T, D1]
+        eff_feat = self.efficientnet_td(inputs, training=training)    # [B, T, D2]
         return xcep_feat, eff_feat
-    
+
     def get_config(self):
         config = super().get_config()
-        config.update({"freeze_ratio": self.freeze_ratio})
+        config.update({
+            "freeze_ratio": self.xception_td.layers[0].trainable,
+            "xception_td": self.xception_td.get_config(),
+            "efficientnet_td": self.efficientnet_td.get_config()
+        })
         return config
-
+    
     @classmethod
     def from_config(cls, config):
         return cls(**config)
--- models/fusion.py
+++ models/fusion.py
@@ -1,23 +1,32 @@
 import tensorflow as tf
 import keras
 from keras.layers import Concatenate, Dense, BatchNormalization, Layer
-from .feature_extractor import FeatureExtractor
 
 @keras.saving.register_keras_serializable(package="Custom")
 class Fusion(Layer):
-    def __init__(self, embed_dims=512, **kwargs):
-        super(Fusion, self).__init__(**kwargs)
+    def __init__(self, embed_dims=256, **kwargs):
+        super().__init__(**kwargs)
         self.embed_dims = embed_dims
         self.concat = Concatenate(axis=-1)
-        self.feature_projection = Dense(self.embed_dims, kernel_initializer='he_normal', dtype='float32')
         self.bn_projection = BatchNormalization()
+        self.feature_projection = None
+
+    def build(self, input_shape):
+        d1 = input_shape[0][-1]
+        d2 = input_shape[1][-1]
+        in_dim = d1 + d2
+
+        self.feature_projection = Dense(
+            self.embed_dims,
+            name="feature_projection"
+        )
+        super().build(input_shape)
 
     def call(self, inputs, training=False):
-        xception_features, efficientnet_features = inputs
-        combined_features = self.concat([xception_features, efficientnet_features]) # [batch_size, 2048 + 1280]
-        combined_features = self.feature_projection(combined_features) # [batch_size, embed_dim]
+        xception_features, efficientnet_features = inputs  # [B, T, D1], [B, T, D2]
+        combined_features = self.concat([xception_features, efficientnet_features])  # [B, T, D1+D2]
+        combined_features = self.feature_projection(combined_features)              # [B, T, embed_dim]
         combined_features = self.bn_projection(combined_features, training=training)
-
         return combined_features
     
     def compute_output_shape(self, input_shape):
