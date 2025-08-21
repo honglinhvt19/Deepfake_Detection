ea1850f92b5d746df5f0198fb5135ceaf1217e8f
honglinhdayne <honglinhvt19@gmail.com>
2025-08-21 22:27:06 +0700

    fix

--- utils/metrics.py
+++ utils/metrics.py
@@ -2,6 +2,26 @@ import tensorflow as tf
 import numpy as np
 from keras.metrics import AUC, Precision, Recall
 
+class F1Score(tf.keras.metrics.Metric):
+    def __init__(self, name="f1", **kwargs):
+        super().__init__(name=name, **kwargs)
+        self.precision = Precision()
+        self.recall = Recall()
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        self.precision.update_state(y_true, y_pred, sample_weight)
+        self.recall.update_state(y_true, y_pred, sample_weight)
+
+    def result(self):
+        p = self.precision.result()
+        r = self.recall.result()
+        return 2 * ((p * r) / (p + r + 1e-7))
+
+    def reset_states(self):
+        self.precision.reset_states()
+        self.recall.reset_states()
+
+
 def get_metrics(metric_names):
     metric_objs = []
     for m in metric_names:
@@ -12,8 +32,6 @@ def get_metrics(metric_names):
             metric_objs.append(Precision(name="precision"))
         elif m == "recall":
             metric_objs.append(Recall(name="recall"))
-        elif m == "f1":
-            metric_objs.append(F1Score(name="f1"))
         elif m == "roc_auc":
             metric_objs.append(AUC(name="roc_auc"))
         elif m == "pr_auc":
