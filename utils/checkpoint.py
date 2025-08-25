3031656dec2c3c2600c2a614fd20954fe6289139
honglinhdayne <honglinhvt19@gmail.com>
2025-08-25 08:18:35 +0700

    update

--- utils/checkpoint.py
+++ utils/checkpoint.py
@@ -9,7 +9,7 @@ from models.fusion import Fusion
 from models.transformer import Transformer
 from models.model import ModelBuilder
 
-def create_checkpoint_callback(checkpoint_dir, monitor='val_roc_auc', mode='max'):
+def create_checkpoint_callback(checkpoint_dir, monitor='roc_auc', mode='max'):
     os.makedirs(checkpoint_dir, exist_ok=True)
     checkpoint_path = os.path.join(checkpoint_dir, f"model_{{epoch:02d}}-{{{monitor}:.4f}}.keras")
     print(f"Saving checkpoints to: {checkpoint_dir}")
@@ -23,7 +23,7 @@ def create_checkpoint_callback(checkpoint_dir, monitor='val_roc_auc', mode='max'
     )
     return checkpoint_callback
 
-def load_checkpoint(model, checkpoint_path, monitor='val_roc_auc'):
+def load_checkpoint(model, checkpoint_path, monitor='roc_auc'):
     if not os.path.exists(checkpoint_path):
         print(f"Can't find checkpoint at {checkpoint_path}")
         checkpoint_callback = create_checkpoint_callback(checkpoint_path, monitor=monitor)
@@ -60,7 +60,7 @@ def load_checkpoint(model, checkpoint_path, monitor='val_roc_auc'):
 
             with custom_object_scope(custom_objects):
                 loaded_model = load_model(latest_checkpoint, custom_objects=custom_objects)
-                print(f"Loaded model from {latest_checkpoint} at epoch {latest_epoch} (val_roc_auc={best_val_auc:.4f})")
+                print(f"Loaded model from {latest_checkpoint} at epoch {latest_epoch} (roc_auc={best_val_auc:.4f})")
 
                 checkpoint_callback = create_checkpoint_callback(checkpoint_path, monitor=monitor)
                 if best_val_auc is not None:
